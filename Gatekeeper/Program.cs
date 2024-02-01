7037b162221cd2f4b7d57ce79b51bfd1fdf68039
V <vegamehra@gmail.com>
2024-02-01 16:53:31 -0500

    Add DbContext scoping

--- Gatekeeper/Program.cs
+++ Gatekeeper/Program.cs
@@ -2,26 +2,29 @@ using Discord.WebSocket;
 using Gatekeeper.Data;
 using Gatekeeper.Services;
 
-var host = Host.CreateDefaultBuilder(args)
+var app = Host.CreateDefaultBuilder(args)
     .ConfigureAppConfiguration(config => config.AddEnvironmentVariables())
     .ConfigureServices(services =>
     {
-        // Backend
-        services.AddDbContext<MemberDbContext>();
-        services.AddSingleton<RepositoryService>();
-        services.AddSingleton<GuildAffiliationService>();
-        services.AddSingleton<MemberService>();
+                // Backend
+        services.AddDbContext<MemberDbContext>(optionsLifetime: ServiceLifetime.Singleton)
+                .AddDbContextFactory<MemberDbContext>()
+                .AddSingleton<RepositoryService>()
+                .AddSingleton<GuildAffiliationService>()
+                .AddSingleton<MemberService>()
+                
+                // Auth
+                .AddSingleton<AuthService>()
+                .AddHostedService<ExpiredCodeCleanerService>()
 
-        // Auth
-        services.AddSingleton<AuthService>();
-        services.AddHostedService<ExpiredCodeCleanerService>();
-
-        // Discord
-        services.AddSingleton<DiscordSocketClient>();
-        services.AddSingleton<DiscordManagementService>();
+                // Discord
+                .AddSingleton<DiscordSocketClient>()
+                .AddHostedService<DiscordManagementService>();
     }).Build();
-    
-using (var scope = host.Services.CreateScope())
-    await scope.ServiceProvider.GetRequiredService<MemberDbContext>().Database.EnsureCreatedAsync();
 
-host.Run();
+using (var scope = app.Services.CreateScope())
+{
+    await scope.ServiceProvider.GetRequiredService<MemberDbContext>().Database.EnsureCreatedAsync();
+}
+
+app.Run();
